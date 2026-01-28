ff88b68aca84842ed6c9e353e25c2481a6339b27
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-29 00:41:55 +0530

    for loop practice 1 and sum caclculate

---
+++ javaScript/1. for-loop.js
@@ -0,0 +1,27 @@
+// for loop =========
+
+for (let count = 1; count <= 5; count++) {
+    console.log("kamaluddin");
+}
+console.log("loop has ended");
+
+
+
+// --calculate SUM 1 to 5
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     sum = sum + i;
+//     console.log("i", i);
+// }
+// console.log(sum);
+
+
+// count-------
+let sum = 0;
+let n = 100;
+for (let j = 1; j <= n; j++) {
+    sum = sum + j
+    console.log(j);
+}
+
+console.log(sum);
