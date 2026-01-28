e2a4966a34929becca696bb7deaeb73d72e38c47
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-29 04:34:32 +0530

    task complete

--- javaScript/6. practice-task.js
+++ javaScript/6. practice-task.js
@@ -18,3 +18,27 @@ for (let num = 0; num <= 100; num++) {
 
 // ===================================
 
+
+/***********************************
+  Qs2.
+   Creat a game where you start with any random game number. Ask the user to keep guessing the game number until the user enters correct value.
+ ***********************************/
+// let gameNum  = 25;
+// let userNum = prompt("Guess the correct number");
+
+// while (userNum != gameNum) {
+//     userNum = prompt("you enter a wrong number : again Guess");
+// }
+
+// console.log("you enter the correct number 25");
+
+
+
+let gameNum = 20;
+let userNum;
+
+do {
+    userNum = prompt("enter correct number");
+} while (gameNum != userNum);
+
+console.log("you entered correct number");
