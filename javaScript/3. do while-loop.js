effd96afc3bcb1be217c223c8e0da66c6eaa026d
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-29 01:24:09 +0530

    while loop practice

---
+++ javaScript/3. do while-loop.js
@@ -0,0 +1,17 @@
+/******************************
+ *  do while loop / run atleast 1 time
+*******************************/
+
+let i = 10; //value big
+do {
+    console.log("kamal");
+    i++;          // atleast 1 time run
+} while (i <= 5);
+
+
+// print 1 to 20 in while loop
+let j = 1;
+do {
+    console.log("j =", j);
+    j++
+} while (j <= 20);
