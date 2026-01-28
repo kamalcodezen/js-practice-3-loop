38b327a77502ac8f58d4782da202a18068e26a77
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-29 01:45:33 +0530

    for of loop practice

---
+++ javaScript/4. for-of-loop.js
@@ -0,0 +1,20 @@
+/******************************
+ *  for of loop / also use string or array
+ *******************************/
+
+// string value print
+let str = "kamaluddin";
+for (let val of str) {
+    console.log("val =", val);
+    val++;
+}
+
+
+// size | length print ......
+let fullName = "apnaCollage";
+let size = 0;
+for (let val of fullName){
+    console.log(val);
+    size++;
+}
+console.log(size);
