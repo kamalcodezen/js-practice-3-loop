ae392687895f50a5adc24439b55fe2dfcf99e38e
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-30 23:56:43 +0530

    while loop syntax practice

---
+++ javaScript/11.while.js
@@ -0,0 +1,19 @@
+
+const nums = 70;
+if (nums <= 10) {
+    console.log(`This is small number`);
+}
+
+console.log(`i'm practice while loop`);
+
+
+// --------WHILE LOOP -------------     
+
+let num = 0;
+while (num < 5) {
+    console.log(`this is while loop `)
+    num++;
+    // num = num + 1;
+    // num += 1;
+}
+console.log(num);
